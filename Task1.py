edaeb8a35011c0c0f322cff683d64928538e94b2
kulbeers8 <104750340+kulbeers8@users.noreply.github.com>
2025-03-01 10:23:54 +0530

    First Assignment
    First Assignment solution

---
+++ Task1.py
@@ -0,0 +1,9 @@
+#Task1 Perform Basic Mathematical Operations
+First_Number=int(input("Enter the first number: "))
+Second_Number=int(input("Enter the second number: "))
+
+print("Addition: ", First_Number+Second_Number)
+print("Subtraction: ", First_Number-Second_Number)
+print("Multiplication: ", First_Number*Second_Number)
+print("Division: ", First_Number/Second_Number)
+

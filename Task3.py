a7e7b2c52c08728d9ecccfcdaa07f1640473e3cf
kulbeers8 <thakurkulbeersingh@gmail.com>
2025-08-02 21:02:38 +0530

    Assignement 2 Task 1

---
+++ Task3.py
@@ -0,0 +1,6 @@
+#This program is use to check number is even or odd.
+number=int(input("Enter a number: "))
+if number%2 == 0:
+  print("{} is an even number.".format(number))
+else:
+  print("{} is an odd number.".format(number))

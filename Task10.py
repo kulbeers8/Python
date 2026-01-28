1392a94a92a198c80eaaf1b0133439ba6a1f8119
kulbeers8 <thakurkulbeersingh@gmail.com>
2026-01-28 15:57:07 +0530

    Assignement 5 Task 2

---
+++ Task10.py
@@ -0,0 +1,6 @@
+actual_list=list(range(1,11))
+firstfive=actual_list[:5]
+reverse=firstfive[::-1]
+print(f"Original list: {actual_list}")
+print(f"Extracted first five elements: {firstfive}")
+print(f"Reversed extracted elements: {reverse}")
